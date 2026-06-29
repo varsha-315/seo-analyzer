9a654c8ee642ae78e59226107af2faa96eea4cad
varsha-315 <varshabaisoya72@gmail.com>
2026-06-29 14:29:29 +0530

    Add reports API route

--- backend/routes/analyzeRoutes.js
+++ backend/routes/analyzeRoutes.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-
 const {
-    analyzeWebsite,
-    getReports
+  analyzeWebsite,
+  getReports
 } = require("../controllers/analyzeController");
 
+// Analyze website
 router.post("/analyze", analyzeWebsite);
 
+// Get all previous reports
+router.get("/reports", getReports);
 
 module.exports = router;
